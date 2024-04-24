21af7c22581818a031286676dbaea9797f5bfb08
Ben Lobaugh <ben.lobaugh@mindsize.com>
2024-04-24 12:03:03 -0700

    Updated Database class to match interface methods

--- src/Eloquent/Database.php
+++ src/Eloquent/Database.php
@@ -73,7 +73,7 @@ class Database implements ConnectionInterface
      *
      * @return \Illuminate\Database\Query\Builder
      */
-    public function table($table)
+    public function table($table, $as = null)
     {
         $processor = $this->getPostProcessor();
 
@@ -96,17 +96,17 @@ class Database implements ConnectionInterface
         return new Expression($value);
     }
 
-	/**
-	 * Get a new query builder instance.
-	 *
-	 * @return \Illuminate\Database\Query\Builder
-	 */
-	public function query()
-	{
-		return new Builder(
-			$this, $this->getQueryGrammar(), $this->getPostProcessor()
-		);
-	}
+    /**
+     * Get a new query builder instance.
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function query()
+    {
+        return new Builder(
+            $this, $this->getQueryGrammar(), $this->getPostProcessor()
+        );
+    }
 
     /**
      * Run a select statement and return a single result.
@@ -469,5 +469,10 @@ class Database implements ConnectionInterface
     public function getConfig($option = null)
     {
         return Arr::get($this->config, $option);
+    }
+
+    public function getDatabaseName()
+    {
+        return DB_NAME;
     }
 }
